7e9a3c45fef1b62aa55442ad0cf85cae1801a9f2
Denny <dennygunawa71@gmail.com>
2016-05-30 10:36:41 +0700

    add folder testing reader pdf files in richtextbox

---
+++ TA/Testing pdf reader in textbox or richtext/Testing reader in textbox or richtext/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+
+namespace Testing_reader_in_textbox_or_richtext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            string filePath;
+            dlg.Filter = "PDF Files(*.PDF)|*.PDF|ALL Files(*.*)|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                filePath = dlg.FileName.ToString();
+
+
+                string strText = string.Empty;
+                try
+                {
+                    PdfReader read = new PdfReader(filePath);
+                    for (int page = 1; page <= read.NumberOfPages; page++)
+                    {
+                        ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                        String s = PdfTextExtractor.GetTextFromPage(read, page, its);
+
+                        s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                        strText = strText + s;
+                        richTextBox1.Text = strText;
+                    }
+                    read.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
