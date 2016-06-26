42c6968d7311e7d6de61ef7b57cf8457a6f3f1a3
rivaldi <rivaldi.leonhart@gmail.com>
2016-06-26 21:21:45 +0700

    stopwords
    done..

---
+++ TA/Stopwords/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(300, 147);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(364, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(769, 147);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(364, 219);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(685, 238);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 487);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
---
+++ TA/Stopwords/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public OleDbConnection conn;
+        public string link = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Kamus.accdb; Persist Security Info=False;";
+        public string sql;
+        public string[] list;
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            //AllocConsole();
+        }
+        //[DllImport("kernel32.dll", SetLastError = true)]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //static extern bool AllocConsole();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Char chr = textBox1.Text[0];
+            string[] word = textBox1.Text.Split('.');
+            
+            foreach (string item in word)
+            {
+                listBox1.Items.Add(item);
+            }
+            sql = "SELECT List FROM StopWord_List";
+            conn = new OleDbConnection(link);
+            stoplist(sql);
+            conn.Open();
+            OleDbCommand com = new OleDbCommand(sql, conn);
+            OleDbDataReader reader = com.ExecuteReader();
+            //int z = word.Length;
+            string[] temp= new string[listBox1.Items.Count];
+            string regexCode = string.Format(@"\s?\b(?:{0})\b\s?", string.Join("|", list));
+            
+            Regex regex = new Regex(regexCode, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            Regex removeDoubleSpace = new Regex(@"\s{2,}", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            for (int i = 0;i < listBox1.Items.Count; i++)
+            {
+                temp[i] = regex.Replace(listBox1.Items[i].ToString(), " ");
+                temp[i] = removeDoubleSpace.Replace(temp[i].ToString(), " ");
+                listBox2.Items.Add(temp[i]);
+            }
+            
+            //temp = removeDoubleSpace.Replace(cleaned, " ");
+
+
+            conn.Close();
+        }
+        public int jlhlist(string query)
+        {
+            conn = new OleDbConnection(link);
+            conn.Open();
+            OleDbCommand comm = conn.CreateCommand();
+            sql = query;
+            comm.CommandText = sql;
+            int jlh = (Int32)comm.ExecuteScalar();
+            conn.Close();
+            return jlh;
+        }
+        public void stoplist(string sql)
+        {
+            conn = new OleDbConnection(link);
+            conn.Open();
+            OleDbCommand comm = conn.CreateCommand();
+            sql = "SELECT List FROM StopWord_List";
+            comm.CommandText = sql;
+            OleDbDataReader reader = comm.ExecuteReader();
+            int jlh = jlhlist("SELECT COUNT (List) FROM StopWord_List");
+            list = new String[jlh];
+            int i = 0;
+            while (reader.Read())
+            {
+                list[i] = reader.GetString(0).ToString();
+                i++;
+            }
+            conn.Close();
+        }
+        public string[] get()
+        {
+            string[] arr = new string[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                arr[i] = listBox1.Items[i].ToString();
+            }
+            return arr;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            string filePath;
+            openDlg.Filter = "PDF Files(*.PDF)|*.PDF|ALL Files(*.*)|*.*";
+
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openDlg.FileName.ToString();
+                string strText = string.Empty;
+                try
+                {
+                    //textBoxInput.Text = File.ReadAllText(openDlg.FileName);
+                    PdfReader read = new PdfReader(filePath);
+                    for (int page = 1; page <= read.NumberOfPages; page++)
+                    {
+                        ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                        String s = PdfTextExtractor.GetTextFromPage(read, page, its);
+
+                        s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                        strText = strText + s;
+                        textBox1.Text = strText;
+                    }
+                    read.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+    }
+}
