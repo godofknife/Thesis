31f4c3aaecc97b1db7e24a7a49072895796822af
godofknife <wisely_jansen@yahoo.com>
2016-07-10 16:41:09 +0700

    Testing Synonym and Levenstein

---
+++ TA/Testing Program/Test Synonym/Test Synonym/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Data.OleDb;
+
+namespace Test_Synonym
+{
+    public partial class Form1 : Form
+    {
+        public OleDbConnection conn;
+        public string link = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Kamus.accdb; Persist Security Info=False;";
+        public string sql;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "PDF Files(*.PDF)|*.PDF|ALL Files(*.*)|*.*";
+
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openDlg.FileName.ToString();
+                filePath = openDlg.FileName.ToString();
+                string strText = string.Empty;
+                try
+                {
+                    //textBoxInput.Text = File.ReadAllText(openDlg.FileName);
+                    PdfReader read = new PdfReader(filePath);
+                    for (int page = 1; page <= read.NumberOfPages; page++)
+                    {
+                        ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                        String s = PdfTextExtractor.GetTextFromPage(read, page, its);
+
+                        s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                        strText = strText + s;
+                        richTextBox1.Text = strText;
+                    }
+                    read.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "PDF Files(*.PDF)|*.PDF|ALL Files(*.*)|*.*";
+
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openDlg.FileName.ToString();
+                filePath = openDlg.FileName.ToString();
+                string strText = string.Empty;
+                try
+                {
+                    //textBoxInput.Text = File.ReadAllText(openDlg.FileName);
+                    PdfReader read = new PdfReader(filePath);
+                    for (int page = 1; page <= read.NumberOfPages; page++)
+                    {
+                        ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                        String s = PdfTextExtractor.GetTextFromPage(read, page, its);
+
+                        s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                        strText = strText + s;
+                        richTextBox2.Text = strText;
+                    }
+                    read.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            Char chr = richTextBox1.Text[0];
+            string[] word = richTextBox1.Text.Split('.');
+            string[] word1 = richTextBox2.Text.Split('.');
+            foreach (string item in word)
+            {
+                listBox1.Items.Add(item);
+
+            }
+            foreach (string item in word1)
+            {
+                listBox2.Items.Add(item);
+            }
+            conn = new OleDbConnection(link);
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Kamus_Tesaurus";
+            conn.Open();
+            int exist = 0;
+            foreach( string x in word)
+            {
+                foreach(string y in word1)
+                {
+                    exist++;
+                }
+            }
+            conn.Close();
+            textBox1.Text = exist.ToString();
+
+        }
+    }
+}
